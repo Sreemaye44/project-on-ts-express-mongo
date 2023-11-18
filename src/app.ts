8d512e24e4d591eba89551c457b46023766e782a
sreemaye <sreemaye44@gmail.com>
2023-11-18 14:04:33 +0600

    gitignore file update

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 const app: Application = express();
-const port = 5000;
 
-app.get("/", (req: Request, res:Response) => {
-  res.send("Hello World!");
+//parsers
+app.use(express.json());
+app.use(cors());
+app.get('/', (req: Request, res: Response) => {
+  const a = 16;
+
+  res.send(a);
 });
-console.log(process.cwd())
+
+console.log(process.cwd());
 
 export default app;
