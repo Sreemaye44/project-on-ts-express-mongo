e5dc845c72e0da9425a64ceab1de47e9ce8bde29
sreemaye <sreemaye44@gmail.com>
2023-11-20 10:16:28 +0600

    joi validation complete

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,29 +1,26 @@
 import { Request, Response, response } from 'express';
 import { StudentServices } from './student.service';
-import Joi from 'joi';
+import studentValidationSchema from './student.validation';
 
 const createStudent = async (req: Request, res: Response) => {
   try {
     //create a schema validation using joi
 
-    const JoivalidationSchema = Joi.object({
-      id: Joi.string(),
-      name: {
-        firstName: Joi.string().max(20).required(),
-        middleName: Joi.string().max(20),
-        lastName: Joi.string().max(20)
-      },
-      gender: Joi.string().required().valid(['male', 'female', ])
-    });
     const { student: studentData } = req.body;
-    console.log(
-      '🚀 ~ file: student.controller.ts:7 ~ createStudent ~ studentData:',
-      studentData,
-    );
+
+    const { error } = studentValidationSchema.validate(studentData);
 
     //will call service function to send this data
 
     const result = await StudentServices.createStudentIntoDB(studentData);
+
+    if (error) {
+      res.status(500).json({
+        success: false,
+        message: 'something went wrong',
+        error: error.details,
+      });
+    }
 
     //send response
     res.status(200).json({
