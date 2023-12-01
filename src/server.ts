37f86e271eb0263471efb54689fb1d50da827bd6
sreemaye <sreemaye44@gmail.com>
2023-12-01 22:49:45 +0600

    env uopdate

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,13 @@ import app from './app';
 import config from './app/config';
 
 import mongoose from 'mongoose';
+const PORT=3000
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port 3000`);
     });
   } catch (error) {
     console.log(error);
