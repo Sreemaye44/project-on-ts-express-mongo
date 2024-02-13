2683b4dfdda2f264bef8eb6072fe0f01ba4de9b2
sreemaye <sreemaye44@gmail.com>
2024-02-13 17:44:36 +0600

    offered course update

--- src/app/modules/OfferedCourse/OfferedCourse.service.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.service.ts
@@ -1,7 +1,36 @@
+import httpStatus from 'http-status';
+import { AppError } from '../../errors/AppError';
+import { SemesterRegistration } from '../semesterRegistration/semesterRegistration.model';
 import { TOfferedCourse } from './OfferedCourse.interface';
 import { OfferedCourse } from './OfferedCourse.model';
+import { AcademicFaculty } from '../academicFaculty/academicFaculty.model';
 
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
+  //check of the semester registrationn id is exist
+  const {
+    semesterRegistration,
+    academicFaculty,
+    academicDepartment,
+    course,
+    faculty,
+  } = payload;
+
+  const isSemesterRegistrationExists =
+    await SemesterRegistration.findById(SemesterRegistration);
+  if (!isSemesterRegistrationExists) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'Semester registration not found!',
+    );
+  }
+  const isAcademicFacultyExists =
+    await AcademicFaculty.findById(academicFaculty);
+  if (!isSemesterRegistrationExists) {
+    throw new AppError(
+      httpStatus.NOT_FOUND,
+      'Semester registration not found!',
+    );
+  }
   const result = await OfferedCourse.create(payload);
   return result;
 };
